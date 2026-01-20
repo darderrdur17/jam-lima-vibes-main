16325faa30541379e495439b7bef7550aaec4bab
darderrdur17 <e0960472@u.nus.edu>
2026-01-20 12:57:05 +0800

    Add build configuration to output directory for documentation

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
